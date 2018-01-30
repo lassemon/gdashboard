1821528963d9803a6692c9c9b0c326f71022f300
Lasse Pirinen <lassemon.pirinen@gmail.com>
2018-01-30 23:59:17 +0200

    move everything inside App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 import { Route } from 'react-router-dom';
+import './index.css';
 import Home from './routes/Home/'
 import Podcasts from './routes/Podcasts/'
 import News from './routes/News/'
 
 class App extends React.Component {
   render(){
-    return [
-        <Route exact path="/" component={Home} key="home"/>,
-        <Route path="/podcasts" component={Podcasts} key="podcasts"/>,
-        <Route path="/news" component={News} key="news"/>
-    ];
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div>
+            <Route exact path="/" component={Home} key="home" />
+            <Route path="/podcasts" component={Podcasts} key="podcasts" />
+            <Route path="/news" component={News} key="news" />
+          </div>
+        </ConnectedRouter>
+      </Provider>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from './store'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-     <ConnectedRouter history={history}>
-         <App />
-     </ConnectedRouter>
-   </Provider>,
+  <App />,
   document.getElementById('root')
 );
 
